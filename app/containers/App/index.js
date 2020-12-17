5196605232d2a1d96c3aa8713b1e610f3292b240
blacktiny <blacktiny126@hotmail.com>
2020-12-17 11:43:10 -0500

    add inital screens

---
+++ app/containers/App/index.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * App.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
+ */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Screens
+import Welcome from '../Welcome';
+import Home from '../Home';
+import ViewController from '../ViewController';
+
+const Stack = createStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Stack.Screen name="ViewController" component={ViewController} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  const ref = React.useRef(null);
+
+  return (
+    <NavigationContainer ref={ref}>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
+
+export default App;
